c7a0a049f617f7e28c7a141f1cbad9a872dd32bb
Samuel Evans <samuelevans@samuels-mbp.home>
2020-05-13 15:41:24 +0100

    two passing tests

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-import requests
-import json
+# import requests
+# import json
 from os import environ
+
+class Weather:
+
+    url = "https://api.openweathermap.org/data/2.5/weather?"
+
+    def __init__(self, key):
+        pass
+
+    def get_weather_json(self):
+        return {"cod":"200"}
+
+    def get_user_input(self):
+        location = input("were r u lookin 4 m8? ")
+        return "London"
--- test_main.py
+++ test_main.py
@@ -1,3 +1,26 @@
-import unittest.mock
+from main import Weather
+from unittest.mock import patch
+import unittest
 
-class TestWeather(unittest.TestCase)
+class TestWeather(unittest.TestCase):
+
+    def setUp(self):
+        self.app = Weather("key")
+
+    def test_api_integration(self):
+        # arrange
+        expected_response = "200"
+        #act
+        actual = self.app.get_weather_json() 
+        # assert
+        self.assertEqual(expected_response, actual["cod"])
+
+    @patch("builtins.input")
+    def test_get_usinput(self, inputy):
+        # arrange
+        inputy.side_effect = ["London"]
+        expected = "London"
+        # act
+        actual = self.app.get_user_input()
+        # assert
+        self.assertEqual(expected, actual)
